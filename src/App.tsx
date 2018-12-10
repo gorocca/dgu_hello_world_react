07f1135cb389d7bac354f513684dbdf07f5e3f43
Gonzalo Romero Castillo <gonzy1235@hotmail.com>
2018-12-10 20:39:08 +0100

    SCSS support added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
 import './App.css';
-
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faStroopwafel} from '@fortawesome/free-solid-svg-icons';
 import logo from './logo.svg';
+
+library.add(faStroopwafel);
 
 class App extends React.Component {
   public render() {
@@ -12,7 +16,7 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, edit <code>src/App.tsx</code> and save to reload. <FontAwesomeIcon icon="stroopwafel"/>
         </p>
       </div>
     );
